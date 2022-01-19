d78c01782cfa706cedd5644d44c99cc6657912ee
Vicente Gutiérrez <gtz.rvera@gmail.com>
2022-01-19 15:04:28 -0600

    🏃‍♂️ add logout method

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -7,11 +7,16 @@ const AuthController = require('../../controllers/auth');
 // Login form
 router.get('/login', AuthController.showLogin);
 
+// Login
 router.post('/login',
-    passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }),
-    (req, res) => res.redirect('/dashboard')
+  passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }),
+  (req, res) => res.redirect('/dashboard')
 );
 
 // Logout
+router.post('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/login');
+});
 
 module.exports = router;
