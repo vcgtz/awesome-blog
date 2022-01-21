90923f9a47af33942f44eb0db9133f6ce40fa562
Vicente Gutiérrez <gtz.rvera@gmail.com>
2022-01-20 22:21:51 -0600

    📚 add category model

---
+++ database/models/CategorySchema.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+const CategorySchema = Schema({
+  name: {
+    type: String,
+    required: [true, 'Category name is required'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Category description is required'],
+  },
+  slug: {
+    type: String,
+    required: [true, 'Category slug is required'],
+  },
+  timestamp
+}, {
+  timestamps: true
+});
+
+module.exports = model('Category', CategorySchema);
