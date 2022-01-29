66c8d68d89855de1010b9cad9b353fd0c4e3bf5e
Vicente Gutiérrez <gtz.rvera@gmail.com>
2022-01-29 11:27:24 -0600

    🩺  move hbs helpers to an individual file

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('./config/passport');
+const registerHbsHelpers = require('./helpers/hbs');
 
 const hbs = require('hbs');
 const router = require('./routes');
@@ -39,24 +40,7 @@ const start = async () => {
   });
 
   hbs.registerPartials(__dirname + '/views/partials');
-  hbs.registerHelper('isEven', val => val % 2 === 0);
-  hbs.registerHelper('isTabActive', (section, currentUrl) => currentUrl === section);
-  hbs.registerHelper('selectOption', (value, optionValue) => value?.toString() === optionValue.toString());
-  hbs.registerHelper('readableDate', date => {
-    if (!date) {
-      return '';
-    }
-
-    const year = date.getFullYear();
-    const month = ((date.getMonth() + 1) + '').padStart(2, '0');
-    const day = date.getDate();
-
-    const hours = (date.getHours() + '').padStart(2, '0');
-    const minutes = (date.getMinutes() + '').padStart(2, '0');
-    const seconds = (date.getSeconds() + '').padStart(2, '0');
-
-    return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
-  });
+  registerHbsHelpers(hbs);
 
   // Routes
   app.use('/', router);
