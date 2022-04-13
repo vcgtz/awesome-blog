ac48adfddee138eea19f45d18e044274b3e9ff4b
Vicente Gutiérrez <gtz.rvera@gmail.com>
2022-04-12 21:23:07 -0500

    📚 Add category page

--- routes/blog/index.js
+++ routes/blog/index.js
@@ -6,6 +6,7 @@ const BlogController = require('../../controllers/blog/blog');
 
 router.get('/', BlogController.index);
 
+router.get('/category/:slug', BlogController.category);
 router.get('/:slug', BlogController.post);
 
 module.exports = router;
